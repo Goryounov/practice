1cbeb729e8061a8178b6f50e1562a2216da5b280
Nikita Goryunov <nikita@imobis.ru>
2024-07-02 20:48:54 +0300

    initial commit

---
+++ backend.py
@@ -0,0 +1,110 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import time
+import asyncpg
+
+pg_settings = {
+    'user': 'test',
+    'password': 'test',
+    'database': 'postgres',
+    'host': 'localhost',
+    'port': '5432',
+    'table_name': 'diagram'
+}
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get('/diagram/aggregate')
+async def diagram_aggregate(from_time, to_time, canvas_width, canvas_height):
+    conn = await asyncpg.connect(
+        user=pg_settings['user'],
+        password=pg_settings['password'],
+        database=pg_settings['database'],
+        host=pg_settings['host'],
+        port=pg_settings['port']
+    )
+
+    from_time = int(from_time)
+    to_time = int(to_time)
+    canvas_width = int(canvas_width)
+    canvas_height = int(canvas_height)
+
+    start_execution_time = time.perf_counter()
+
+    min_max_query = f"""
+        WITH filtered_data AS (
+            SELECT *
+            FROM {pg_settings['table_name']}
+            WHERE timestamp BETWEEN {from_time} AND {to_time}
+        )
+        SELECT MAX(x) AS max_x,
+            MIN(x) AS min_x,
+            MAX(y) AS max_y,
+            MIN(y) AS min_y
+        FROM filtered_data
+    """
+    
+    bounds = await conn.fetchrow(min_max_query)
+    max_x = bounds['max_x']
+    min_x = bounds['min_x']
+    max_y = bounds['max_y']
+    min_y = bounds['min_y']
+
+    sql_query = f"""
+        WITH filtered_data AS (
+            SELECT *
+            FROM {pg_settings['table_name']}
+            WHERE timestamp BETWEEN {from_time} AND {to_time}
+        ), paddings AS (
+            SELECT ROUND({min_x} / (({max_x} - {min_x}) / {canvas_width})) AS padding_x,
+                ROUND({min_y} / (({max_y} - {min_y}) / {canvas_height})) AS padding_y
+        )
+        SELECT DISTINCT ON (
+                ROUND(x / (({max_x} - {min_x}) / {canvas_width})) - paddings.padding_x, 
+                ROUND(y / (({max_y} - {min_y}) / {canvas_height})) - paddings.padding_y)
+            x,
+            y,
+            ROUND(x / (({max_x} - {min_x}) / {canvas_width})) - paddings.padding_x AS pixel_x,
+            ROUND(y / (({max_y} - {min_y}) / {canvas_height})) - paddings.padding_y AS pixel_y
+        FROM filtered_data,
+            paddings
+    """
+    
+    records = await conn.fetch(sql_query)
+
+    end_execution_time = time.perf_counter()
+    elapsed_time = end_execution_time - start_execution_time
+
+    await conn.close()
+
+    return {"records": records, "query_execution_time": elapsed_time, "bounds": bounds}
+
+
+@app.get('/diagram')
+async def diagram(from_time, to_time):
+    conn = await asyncpg.connect(**pg_settings)
+
+    from_time = int(from_time)
+    to_time = int(to_time)
+
+    start_execution_time = time.perf_counter()
+
+    sql_query = f"""
+        SELECT * FROM {pg_settings['table_name']} WHERE timestamp BETWEEN {from_time} AND {to_time}
+    """
+    records = await conn.fetch(sql_query)
+
+    end_execution_time = time.perf_counter()
+    elapsed_time = end_execution_time - start_execution_time
+
+    await conn.close()
+
+    return {"records": records, "query_execution_time": elapsed_time}
---
+++ generator.py
@@ -0,0 +1,66 @@
+import asyncpg
+import numpy as np
+from datetime import datetime
+import asyncio
+
+pg_settings = {
+    'user': 'test',
+    'password': 'test',
+    'database': 'postgres',
+    'host': 'localhost',
+    'port': '5432',
+    'table_name': 'diagram'
+}
+
+async def create_table(conn, table_name):
+    create_table_query = f"""
+    CREATE TABLE IF NOT EXISTS {table_name} (
+        timestamp BIGINT NOT NULL,
+        x DOUBLE PRECISION NOT NULL,
+        y DOUBLE PRECISION NOT NULL
+    );
+    """
+    await conn.execute(create_table_query)
+
+async def insert_data():
+    start_date = datetime(2024, 1, 1)
+    end_date = datetime(2024, 1, 30)
+    total_seconds = int((end_date - start_date).total_seconds())
+
+    timestamps = np.arange(int(start_date.timestamp()), int(end_date.timestamp()), 1)
+
+    conn = await asyncpg.connect(
+        user=pg_settings['user'],
+        password=pg_settings['password'],
+        database=pg_settings['database'],
+        host=pg_settings['host'],
+        port=pg_settings['port']
+    )
+
+    await create_table(conn, pg_settings['table_name'])
+
+    batch_size = 10000
+    for start_idx in range(0, total_seconds, batch_size):
+        end_idx = min(start_idx + batch_size, total_seconds)
+        batch_timestamps = timestamps[start_idx:end_idx]
+
+        num_samples = end_idx - start_idx
+        x_base = np.linspace(start_idx, end_idx, num_samples)
+        y_base = np.linspace(start_idx, end_idx, num_samples)
+
+        noise_amplitude = batch_size
+        x = x_base + np.random.uniform(-noise_amplitude, noise_amplitude, num_samples)
+        y = y_base + np.random.uniform(-noise_amplitude, noise_amplitude, num_samples)
+
+        records = [(int(batch_timestamps[i]), float(x[i]), float(y[i])) for i in range(end_idx - start_idx)]
+        
+        values = ', '.join(f"({record[0]}, {record[1]}, {record[2]})" for record in records)
+        insert_query = f"INSERT INTO diagram (timestamp, x, y) VALUES {values}"
+        
+        await conn.execute(insert_query)
+
+    await conn.close()
+
+    print("Data inserted into PostgreSQL table successfully.")
+
+asyncio.run(insert_data())
